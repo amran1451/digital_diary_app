b08ad28e628a839c296332226c22c5adb9a836ac
AMIR <aeamarda@gmail.com>
2025-06-16 11:00:30 +0300

    поменял версию в android/app/build.gradle чтобы не ломать установленную

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     ndkVersion = "27.0.12077973"
 
     defaultConfig {
-        applicationId = "com.example.digital_diary_app"
+        applicationId = "com.example.digital_diary_app.debug"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
