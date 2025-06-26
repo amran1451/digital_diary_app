0f5337b5bdb2752f770616475e9b5b42d7c2b3f8
amran1451 <aeamarda@gmail.com>
2025-06-26 10:26:41 +0300

    немного поменял цвета

--- packages/app_theme/lib/src/colors.dart
+++ packages/app_theme/lib/src/colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF054201);
 const Color secondaryColor = Color(0xFF008E30);
 const Color backgroundColor = Color(0xFFF5F5F5);
-const Color darkBackgroundColor = Color(0xFF1F1F1F);
+const Color darkBackgroundColor = Color(0xFF202020);
 
 const Color textColor = Color(0xFFFFFFFF);
 const Color iconColor = Color(0xFFFFFFFF);
