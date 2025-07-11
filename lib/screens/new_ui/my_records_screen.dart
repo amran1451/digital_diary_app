54cfb390e98d13174b122ca891c6eff654da1053
amran1451 <aeamarda@gmail.com>
2025-07-11 16:20:34 +0300

    feat: исправление ошибок компиляции

--- lib/screens/new_ui/my_records_screen.dart
+++ lib/screens/new_ui/my_records_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart' show ScrollDirection;
 
 import '../../main.dart';
 import '../../models/entry_data.dart';
